031effd5e3cda9fb6cf0882d7db2caf98ea7f952
nightSardan <Tpleme@hotmail.com>
2021-01-25 21:47:38 +0000

    Added projects resources

--- src/js/view/projectView.js
+++ src/js/view/projectView.js
@@ -5,8 +5,12 @@ define(['controller/tabController'], function (tabController) {
     externals.show = function (onclick) {
 
         $('.content').empty();
-        $('.content').append($('<h1>Test Projects</h1>'));
+        $('.content').append($('<h1>Projects</h1>'));
+        $('.content').append($('<div class="project">').append($('<img src="resources/Keep_it_clean.png" width="300" height="200"></img>')))
+        $('.content').append($('<div class="project">').append($('<img src="resources/Qubo.png" width="300" height="200"></img>')))
+        $('.content').append($('<div class="project">').append($('<img src="resources/500Miles.png" width="300" height="200"></img>')))
 
+        
         tabController.init(onclick);
     };
 
