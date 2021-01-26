c82e9ae946d5491a359d6697610fd50b5966799c
nightSardan <Tpleme@hotmail.com>
2021-01-26 09:43:15 +0000

    Project tab

--- src/js/view/projectView.js
+++ src/js/view/projectView.js
@@ -6,9 +6,9 @@ define(['controller/tabController'], function (tabController) {
 
         $('.content').empty();
         $('.content').append($('<h1>Projects</h1>'));
-        $('.content').append($('<div class="project">').append($('<img src="resources/Keep_it_clean.png" width="300" height="200"></img>')))
-        $('.content').append($('<div class="project">').append($('<img src="resources/Qubo.png" width="300" height="200"></img>')))
-        $('.content').append($('<div class="project">').append($('<img src="resources/500Miles.png" width="300" height="200"></img>')))
+        $('.content').append($('<div class="project">').append($('<img src="resources/Keep_it_clean.png" width="300" height="200"></img>')).append($('<h3>Keep it Clean</h3>')))
+        $('.content').append($('<div class="project">').append($('<img src="resources/Qubo.png" width="300" height="200"></img>')).append($('<h3>Keep it Clean</h3>')))
+        $('.content').append($('<div class="project">').append($('<img src="resources/500Miles.png" width="300" height="200"></img>')).append($('<h3>Keep it Clean</h3>')))
 
         
         tabController.init(onclick);
